ca6c0acadbc36994c86814c215259ab75492b50e
Dezang <dezang@dezang.net>
2021-08-21 18:55:09 +0900

    chore(restdoc): initial commit

---
+++ restdoc-openapi/src/main/java/net/dezang/restdocopenapi/RestdocOpenapiApplication.java
@@ -0,0 +1,13 @@
+package net.dezang.restdocopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestdocOpenapiApplication.class, args);
+    }
+
+}
---
+++ restdoc-openapi/src/test/java/net/dezang/restdocopenapi/RestdocOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dezang.restdocopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
